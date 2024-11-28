cca40abc78d3b2914c9a19b645d0c5fec870c023
Michael Parque <michael.t.parque@outlook.com>
2024-11-28 18:55:09 +0530

    chore: remove dummyclient and dummyserver

--- build.sh
+++ build.sh
@@ -7,10 +7,6 @@ echo "building lsptrace..."
 pushd lsptrace
 go build -o ../bin .
 popd
-echo "building dummyclient..."
-go build -o bin ./dummyclient/dummy-client.go
-echo "building dummyserver..."
-go build -o bin ./dummyserver/dummy-server.go
 
 echo "successful build"
 
